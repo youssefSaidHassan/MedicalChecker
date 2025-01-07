1e754f4c59360c41729c406908a92a0749a9d780
Youssef Said Hassan <youssef.rsys@gmail.com>
2025-01-07 03:29:49 +0200

    Cofigure ValidationBehavior and mediator in services

--- MedicalChecker.Core/ModuleCoreDependencies.cs
+++ MedicalChecker.Core/ModuleCoreDependencies.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-
+﻿using FluentValidation;
+using MediatR;
+using MedicalChecker.Core.Behaviors;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 namespace MedicalChecker.Core
 {
     public static class ModuleCoreDependencies
     {
         public static IServiceCollection AddCoreDependencies(this IServiceCollection services)
+
         {
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
+
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
             return services;
         }
     }
